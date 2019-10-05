9b10f8db7b3a73ca2189c6aa392edc7ca6d03e27
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:49:20 +0200

    initial commit: added test.js with some simple arrays

---
+++ test.js
@@ -0,0 +1,21 @@
+var myArray = []; //empty  array //
+myArray[0] = 35;
+myArray[1] = 25;
+myArray[2] = 45;
+myArray[3] = "hello";
+console.log(myArray);
+
+
+var myArray2 = [25, 35, false, "hello"];
+
+console.log(myArray2);
+
+var myArray3 = new Array()
+
+var myArray4 = new Array (5);
+
+console.log(myArray4.length);
+console.log(myArray2.length);
+
+console.log(myArray2.sort());
+console.log(myArray2.reverse());
